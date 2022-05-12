0c16fe7a5982910ae8bc2152f06796c5e8924c4b
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-05-12 16:04:34 +0300

    created needed files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import './App.css';
+import React, { Component } from 'react';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  render() {
+    // const { filter } = this.state;
+
+    return (
+      <>
+        <div>wtf</div>
+      </>
+    );
+  }
+}
+
+export default App;
